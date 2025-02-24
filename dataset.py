601621e48e4e3f1bdcb6e313daadb05b676596a1
chril-wen <xieyiwen_1008@126.com>
2025-02-24 19:30:04 +0800

    dataset-change

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-ds = load_dataset("albertvillanova/medmnist-v2", "pathmnist")    # download & load the dataset
+ds = load_dataset("albertvillanova/medmnist-v2", "pathmnist",data_dir="./image")    # download & load the dataset
 
 if __name__ == "__main__":
     labels = ds["train"]["label"]    # get the labels
