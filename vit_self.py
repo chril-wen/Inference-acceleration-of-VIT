2066bcb056c66401e36e0d780b46f0536cf3d60b
chril-wen <xieyiwen_1008@126.com>
2025-03-21 10:48:59 +0800

    =====

--- vit_self.py
+++ vit_self.py
@@ -142,6 +142,7 @@ if __name__ == "__main__":
 
     preds = model_vit(img) 
     print(preds)
+    print("===================================================")
 
     print(preds.shape)  # (16, 1000)
 
